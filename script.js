2224afcb02c903d46f1693fc3dc6ca479127a111
Tech-A08 <tech-a08@Tech-A08s-MacBook-Air.local>
2017-11-02 21:09:52 +0000

    added check for turn change

--- script.js
+++ script.js
@@ -5,15 +5,47 @@ $(function(){
   // $('td').on('click',function(){
   //   $('td').addClass('X').html("X");
   // })
+  var check = true;
+  // var noughtCheck = false;
 
 
-  $('td').each(function (index) {
+  function game() {
+    for (var i = 0; i < 10; i ++) {
+      turnCheck();
+    }
+  }
 
-        $('td').click(function(){
-          $(this).addClass('X').html("X");
-        })
+  game();
 
+  function turnCheck() {
+    if (check === true) {
+      changeCross();
+      check = false;
+    } else if (check === false) {
+      changeNought();
+      check = true;
+    }
+  }
+
+  // turnCheck();
+
+  function changeCross() {
+    // event to change td to cross
+    $('td').each(function (index) {
+      $('td').click(function(event){
+        $(this).addClass('X').html("X");
       })
+    })
+  }
+
+  function changeNought() {
+    // event to change td to nought
+    $('td').each(function (index) {
+      $('td').click(function(event){
+        $(this).addClass('O').html("O");
+      })
+    })
+  }
 
 
 
