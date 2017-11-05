da77833f26993a8cbb5b91da143cb595a90455d4
Tech-A08 <tech-a08@Tech-A08s-MacBook-Air.local>
2017-11-05 16:27:27 +0000

    added reset board function

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ $(function(){
   var $winningCombinations = $([[0,1,2],[3,4,5],[6,7,8],
   [0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]);
 
+  resetListener();
+  listeners();
   // function to set up listeners
   function listeners() {
     $('td').click(function(event){
@@ -16,12 +18,12 @@ $(function(){
       counter ++;
       if ($(this).html().length === 0) {
         if (counter % 2 === 0) {
-          $(this).addClass('X').html("X");
+          $(this).attr("class", "X").html("X");
             XMoves.push(parseInt($(this).attr('data-num')));
             console.log("--- x", XMoves);
             checkForWin(XMoves, "Crosses");
         } else {
-          $(this).addClass('O').html("O");
+          $(this).attr("class", "O").html("O");
             OMoves.push(parseInt($(this).attr('data-num')));
             console.log("--- o", OMoves);
             checkForWin(OMoves, "Noughts");
@@ -42,13 +44,21 @@ $(function(){
         // If counter gets to we have a winning combination
         if(winCounter === 3){
           alert("Game over, " + name + " wins!");
-          // resetBoard();
+          resetBoard();
         }
       })
     })
   }
 
+  function resetListener() {
+    $('#reset').click(resetBoard);
+  }
 
-
-  listeners();
+  function resetBoard() {
+    $('td').attr("class", "clear").html("");
+    winCounter = 0;
+    counter = 0;
+    OMoves = [];
+    XMoves = [];
+  }
 })
