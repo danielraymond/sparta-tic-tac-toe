a69f76a8dd00e12c4309fb1b55d93e4e07ea53fb
Tech-A08 <tech-a08@Tech-A08s-MacBook-Air.local>
2017-11-05 15:12:43 +0000

    tidied up code and added comments and function to check for win

--- script.js
+++ script.js
@@ -1,38 +1,33 @@
 $(function(){
 
-
-
+  // global variables to save entered values
   var counter = 0;
   var oMoves = [];
   var xMoves = [];
 
+  // function to set up listeners
   function listeners() {
-
-      $('td').click(function(event){
-
-            counter ++;
-            if (counter % 2 === 0) {
-              $(this).addClass('X').html("X");
-              xMoves.push($(this).attr('data-num'));
-              console.log("--- x", xMoves);
-              checkForWin(xMoves);
-            } else {
-              $(this).addClass('O').html("O");
-              oMoves.push($(this).attr('data-num'));
-              console.log("--- o", oMoves);
-            }
-            // if ($(this).hasClass( "X" ) === true) {
-            //   var z = 2;
-            //   console.log("class " + z);
-            // } else if ($(this).hasClass( "X" ) === false) {
-            //   var y = 1;
-            //   console.log("class " + y);
-            // }
-
-
+    $('td').click(function(event){
+      // counter to change turn
+      counter ++;
+      if (counter % 2 === 0) {
+        $(this).addClass('X').html("X");
+          xMoves.push($(this).attr('data-num'));
+          console.log("--- x", xMoves);
+          checkForWin(xMoves);
+      } else {
+        $(this).addClass('O').html("O");
+          oMoves.push($(this).attr('data-num'));
+          console.log("--- o", oMoves);
+          checkForWin(oMoves);
+      }
     })
   }
 
+  // function to check who wins
+  function checkForWin(movesArray) {
+
+  }
 
 
   listeners();
