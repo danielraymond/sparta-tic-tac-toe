9257e0bfb950d33234127e0bdebd6d63ee079e09
Tech-A08 <tech-a08@Tech-A08s-MacBook-Air.local>
2017-11-05 13:09:24 +0000

    edited function to change turn and added arrays for saving moves

--- script.js
+++ script.js
@@ -3,28 +3,36 @@ $(function(){
 
 
   var counter = 0;
+  var oMoves = [];
+  var xMoves = [];
 
   function listeners() {
-    $('td').each(function (index) {
+
       $('td').click(function(event){
-          for (var i = 1; i < 10; i ++) {
+
             counter ++;
             if (counter % 2 === 0) {
               $(this).addClass('X').html("X");
+              xMoves.push($(this).attr('data-num'));
+              console.log("--- x", xMoves);
+              checkForWin(xMoves);
             } else {
               $(this).addClass('O').html("O");
+              oMoves.push($(this).attr('data-num'));
+              console.log("--- o", oMoves);
             }
-            if ($(this).hasClass( "X" ) === true) {
-              var z = 2;
-              console.log("class " + z);
-            } else if ($(this).hasClass( "X" ) === false) {
-              var y = 1;
-              console.log("class " + y);
-            }
-          }
-      })
+            // if ($(this).hasClass( "X" ) === true) {
+            //   var z = 2;
+            //   console.log("class " + z);
+            // } else if ($(this).hasClass( "X" ) === false) {
+            //   var y = 1;
+            //   console.log("class " + y);
+            // }
+
+
     })
   }
+
 
 
   listeners();
